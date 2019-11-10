1673d57da9d6e81aa5933c273a2ef6603f9da269
BuildTools <willebergh@outlook.com>
2019-11-10 22:52:57 +0100

    added database connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
 require("dotenv").config();
 
+mongoose.connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if (err) throw err;
+    console.log("Connected to Database");
+})
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port} | APP_STATE: ${process.env.APP_STATE}`)
+    console.log(`Server is running on http://localhost:${port} | PROD_STATE: ${process.env.PROD_STATE}`)
 })
